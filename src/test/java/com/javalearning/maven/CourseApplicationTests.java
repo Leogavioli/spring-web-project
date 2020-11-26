875c12149989f84b3faebb8fc1cd17dc18df5516
Leogavioli <gaviolileo@gmail.com>
2020-11-26 08:51:23 -0300

    Project Created

---
+++ src/test/java/com/javalearning/maven/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
